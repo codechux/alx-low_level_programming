4aad5caf3bd76e4890f7b6915e6bd604b3f875eb
chux <chuxoguh@gmail.com>
2023-02-18 12:29:21 -0800

    printing in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print the last digit
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+	while (i <= 'z')
+		putchar(i);
+	i++;
+	putchar('\n');
+	return (0);
+}
