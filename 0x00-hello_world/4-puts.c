3cffb883cefcd77e5b8fb2060e0c1bfdca981367
chux <chuxoguh@gmail.com>
2023-02-18 05:00:03 -0800

    using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/*
+ * function takes a string as an argument and prints it to the console
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
