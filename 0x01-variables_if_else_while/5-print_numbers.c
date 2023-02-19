1e95a91cf088d252e48c4537cb7e232319779fdd
chux <chuxoguh@gmail.com>
2023-02-19 15:36:56 -0800

    printing base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print lowercase alphabets and uppercase
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
