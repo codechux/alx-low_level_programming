5635d54fef294db76380b89a64bdc8268b8d9821
chux <chuxoguh@gmail.com>
2023-02-22 16:09:27 -0800

    printing the alphabelt 10 times using nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower
+ *
+ * Return : Always return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
