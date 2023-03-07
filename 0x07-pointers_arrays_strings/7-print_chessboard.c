f0d518525d5b8cd49ddddb82d98e24c689c98b15
chux <chuxoguh@gmail.com>
2023-03-07 05:46:03 -0800

    printing a chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: pointer to 2D array
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; ++row)
+	{
+		for (col = 0; col < 8; ++col)
+			putchar(a[row][col]);
+		putchar('\n');
+	}
+}
