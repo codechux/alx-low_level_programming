1598525a24434f889a2de20809ef8b6efe821417
chux <chuxoguh@gmail.com>
2023-03-14 07:39:39 -0700

    adds tow argument and prints the result

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+i
+/**
+ * main - prints the addition of two arguments
+ * @argc: counts the arguments
+ * @argv: arrays of  strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
