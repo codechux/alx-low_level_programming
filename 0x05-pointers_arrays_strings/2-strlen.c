2965786c2a3de7a5cdaa77a7222901bef2631f31
chux <chuxoguh@gmail.com>
2023-05-29 13:25:13 -0700

    betty compliant

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @str: string to be processed
+ * Return: lenth of string
  */
 
 int _strlen(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 	return (i);
 }
