ff5d300aa5f4c075b1ea14fd96631690eda16355
chux <chuxoguh@gmail.com>
2023-05-29 13:46:19 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string
+ * @str: string to be processed
+ */
+void print_rev(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = i - 1; j >= 0; j--)
+			_putchar(str[j]);
+	_putchar('\n');
+}
