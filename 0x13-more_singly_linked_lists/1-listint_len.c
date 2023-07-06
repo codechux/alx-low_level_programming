c43232848f3a890b999cd592f012023fce2679b2
chux <chuxoguh@gmail.com>
2023-07-05 19:47:43 -0700

    fixing betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,19 +3,17 @@
 /**
  * listint_len - counts the number of elements in a linked list
  * @h: head of the list
- *
  * Return: the number of nodes in the listint_t list.
  */
 
- size_t listint_len(const listint_t *h)
- {
- 	size_t count = 0;
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
 
 	while (h)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
