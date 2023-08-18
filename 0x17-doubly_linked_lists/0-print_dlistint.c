24698ad266720f3fa774205894faf50f994af3f9
chux <chuxoguh@gmail.com>
2023-08-18 04:21:01 -0700

    betty requirement 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *element = h;
 
-	while(element != NULL)
+	while (element != NULL)
 	{
 		printf("%d\n", element->n);
 		element = element->next;
