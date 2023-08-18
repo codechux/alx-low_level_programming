1d5e2bedea72599efdd89f45bbe2758ab1d24e69
chux <chuxoguh@gmail.com>
2023-08-18 04:19:09 -0700

    betty requirement

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- *
+ * print_dlistint - function to prints elements
+ * dlistint - node in the list
+ * Return: number of elements
  */
 
 size_t print_dlistint(const dlistint_t *h)
@@ -15,5 +17,5 @@ size_t print_dlistint(const dlistint_t *h)
 		element = element->next;
 		count++;
 	}
-	return count;
+	return (count);
 }
